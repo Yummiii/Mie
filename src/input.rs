72873202d6f8e7ad07f58292f4fc313e1317779c
Yummiii <Leeo05050@protonmail.com>
2021-10-25 00:21:07 -0300

    deve tar funcionando

--- src/input.rs
+++ src/input.rs
@@ -1,9 +1,9 @@
-use evdev::{Device, InputEventKind};
-use run_script::ScriptOptions;
 use std::thread;
 use tokio::sync::mpsc::Sender;
 
+#[cfg(not(target_family = "windows"))]
 pub fn ler_input(tx: Sender<(bool, u16)>) {
+    use evdev::{Device, InputEventKind};
     thread::spawn(move || {
         let mut device = Device::open(format!("/dev/input/event{}", get_keyboard())).unwrap();
         loop {
@@ -19,7 +19,9 @@ pub fn ler_input(tx: Sender<(bool, u16)>) {
     });
 }
 
+#[cfg(not(target_family = "windows"))]
 pub fn get_keyboard() -> i32 {
+    use run_script::ScriptOptions;
     let (code, output, _) = run_script::run(r#"grep -E 'Handlers|EV=' /proc/bus/input/devices | grep -B1 'EV=120013' | grep -Eo 'event[0-9]+' | grep -Eo '[0-9]+' | tr -d '\n'"#, &vec![], &ScriptOptions::new()).unwrap();
     if code == 0 {
         output.parse::<i32>().unwrap()
