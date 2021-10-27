8888bfe341e0743bd0cb7e2cdd24d370badb0f07
Yummiii <Leeo05050@protonmail.com>
2021-10-27 09:36:29 -0300

    testes fodas.exe

--- src/estruturas.rs
+++ src/estruturas.rs
@@ -3,6 +3,6 @@ use serde::{Serialize, Deserialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Tecla {
     pub pressed: bool,
-    pub key: u16,
+    pub key: u8,
     pub special: bool
 }
