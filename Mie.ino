f74fce469ad034d32085bb6e357fbec3c13b11a2
Yummiii <Leeo05050@protonmail.com>
2021-10-27 10:33:21 -0300

    codigo do arduino foda

--- Mie.ino
+++ Mie.ino
@@ -12,10 +12,20 @@ byte buf[3];
 void loop() {
   if (Serial.available() > 0) {
     Serial.readBytes(buf, 3);
-    Serial.println(buf[0]); 
-    Serial.println(buf[1]); 
-    Serial.println(buf[2]);
-    
+
+    if (buf[0] == 0) {
+      if (buf[1] == 0) {
+        keyboard.keyboard_release(buf[3]);
+       } else {
+        keyboard.keyboard_release_special(buf[3]);
+       }
+    } else {
+        if (buf[1] == 0) {
+          keyboard.keyboard_press(buf[3]);
+       } else {
+          keyboard.keyboard_press_special(buf[3]);
+       }
+    }
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(100);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
